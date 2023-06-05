f4064cc911fccbe7cc994c5693acd6f7f16c5c4c
Gerson Ricardo Navarro Hernández <gersonup@gmail.com>
2023-06-05 05:29:37 -0500

    creating navigation bar

--- app/src/main/java/com/example/marketapp/HomeActivity.kt
+++ app/src/main/java/com/example/marketapp/HomeActivity.kt
@@ -1,15 +1,73 @@
 package com.example.marketapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import com.example.marketapp.databinding.ActivityHomeBinding
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
+
 
 enum class ProviderType {
     BASIC
 }
-class HomeActivity : AppCompatActivity() {
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private  lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this,drawer,toolbar, R.string.open,R.string.close)
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when (item.itemId) {
+           R.id.nav_firstItem -> Toast.makeText(this,"item 1", Toast.LENGTH_SHORT).show()
+           R.id.nav_secondItem -> Toast.makeText(this,"item 2", Toast.LENGTH_SHORT).show()
+           R.id.nav_thirdItem-> Toast.makeText(this,"item 3", Toast.LENGTH_SHORT).show()
+       }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
